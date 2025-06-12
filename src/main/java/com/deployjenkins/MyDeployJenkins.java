31b277f1d0afce9fc1cecb447bc4de53c6e8380d
MonikaDoodleblue <monika.doodleblue@gmail.com>
2025-06-12 10:28:55 +0530

    new getmapping added

--- src/main/java/com/deployjenkins/MyDeployJenkins.java
+++ src/main/java/com/deployjenkins/MyDeployJenkins.java
@@ -16,4 +16,18 @@ public class MyDeployJenkins {
 		return "Hello";
 	}
 
-} 
+	@GetMapping("/get3")
+	public String palindrome() {
+		String a = "mom";
+		String b = "";
+		for (int i = a.length() - 1; i >= 0; i--) {
+			b += a.charAt(i);
+		}
+		if (b.equalsIgnoreCase(a)) {
+			return "is palindrome";
+		} else {
+			return "not palindrome";
+		}
+	}
+
+}
